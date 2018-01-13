c27b35da703cc2350918540c83d8e8b1beb52f57
Ian Lesperance <ian@elliterate.com>
2018-01-13 11:52:07 -0500

    Store working source on a persistent, mounted volume

--- workspace/recipes/src_directory.rb
+++ workspace/recipes/src_directory.rb
@@ -1,5 +1,13 @@
-directory node['workspace']['src_directory'] do
-  user node['workspace']['user']
+include_recipe 'workspace::volume'
+
+persistent_src_directory = ::File.join(node['workspace']['volume']['mount_point'], 'src')
+
+directory persistent_src_directory do
+  owner node['workspace']['user']
   recursive true
-  mode 0755
+end
+
+link node['workspace']['src_directory'] do
+  to persistent_src_directory
+  owner node['workspace']['user']
 end
---
+++ workspace/recipes/volume.rb
@@ -0,0 +1,16 @@
+volume_options = node['workspace']['volume']
+
+directory volume_options['mount_point'] do
+  recursive true
+end
+
+[:enable, :mount].each do |mount_action|
+  mount volume_options['mount_point'] do
+    action mount_action
+    device volume_options['device']
+    dump 0
+    fstype volume_options['fstype']
+    options ['defaults', 'discard', 'nofail']
+    pass 0
+  end
+end
