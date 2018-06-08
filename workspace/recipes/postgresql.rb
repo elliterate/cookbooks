265a4ce9586b82717408e5419b4cdfec8372675f
Ian Lesperance <ian@elliterate.com>
2018-06-08 21:35:52 +0000

    Install PostgreSQL server

--- workspace/recipes/postgresql.rb
+++ workspace/recipes/postgresql.rb
@@ -1,1 +1,21 @@
 postgresql_client_install 'client'
+
+postgresql_server_install 'server'
+
+postgresql_user node['workspace']['user'] do
+  user node['workspace']['user']
+  superuser true
+end
+
+postgresql_access node['workspace']['user'] do
+  access_type 'local'
+  access_db 'all'
+  access_user node['workspace']['user']
+  access_method 'ident'
+end
+
+postgresql_ident node['workspace']['user'] do
+  mapname node['workspace']['user']
+  system_user node['workspace']['user']
+  pg_user node['workspace']['user']
+end
